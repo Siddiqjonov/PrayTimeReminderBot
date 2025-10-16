d8a574dcec53a7ac2f1dff22bbe015ddac55ee6c
Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-10-16 08:21:16 +0500

    mainContext updated

--- PrayTimeBot.Infrastructure/MainContext.cs
+++ PrayTimeBot.Infrastructure/MainContext.cs
@@ -9,16 +9,14 @@ public class MainContext : DbContext
     public DbSet<User> Users => Set<User>();
     public DbSet<PrayerTime> PrayerTimes => Set<PrayerTime>();
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        options.UseNpgsql("Host=ballast.proxy.rlwy.net;Port=57464;Database=railway;Username=postgres;Password=yiAeXngHQbHvpSImHDjoTjZDMEqCnKVt");
+        var conn = Environment.GetEnvironmentVariable("DATABASE_URL")?.Trim();
 
-        //var conn = Environment.GetEnvironmentVariable("DATABASE_URL");
+        if (string.IsNullOrEmpty(conn))
+            Console.WriteLine("❌ DATABASE_URL not found. Please set it in Railway Variables.");
 
-        //if (string.IsNullOrEmpty(conn))
-        //    throw new Exception("DATABASE_URL not found. Please set it in Railway Variables.");
-
-        //options.UseNpgsql(conn);
+        optionsBuilder.UseNpgsql(conn);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
