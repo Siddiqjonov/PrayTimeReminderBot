e2caff282a3bb7767c2fc5de31c2f5193f569073
Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-10-16 09:29:33 +0500

    bug fixed

---
+++ PrayTimeBot.Infrastructure/Migrations/20251016042923_UserEntityNewCreatedAt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrayTimeBot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserEntityNewCreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "TO_CHAR(NOW(), 'YYYY-MM-DD HH24:MI:SS')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "TO_CHAR(NOW(), 'YYYY-MM-DD HH24:MI:SS')",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+    }
+}
